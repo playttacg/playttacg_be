cd2bd50fc5029c752fd6dc51b74750edd3d74400
truefuego <ankitsharma15092003@gmail.com>
2025-01-26 17:10:27 +0530

    Fix - connection to mongodb

--- config/db.connection.js
+++ config/db.connection.js
@@ -1,19 +1,17 @@
-// config/db.connection.js
 import mongoose from "mongoose";
 
-const connectMongoDB = async () => {
-  try {
-    if (!process.env.DB_URI) {
-      throw new Error("DB_URI is not defined in the environment variables");
-    }
+const connect_to_db = async () => {    
+  if (!process.env.DB_URI) {
+    throw new Error("DB_URI is not defined in the environment variables");
+  }
 
-    console.log("Connecting to MongoDB...");
-    await mongoose.connect(process.env.DB_URI);
-    console.log("Connected to MongoDB successfully!");
+  try {
+    const conn = await mongoose.connect(process.env.DB_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error("Error connecting to MongoDB:", error.message || error);
-    process.exit(1); // Exit process if the database connection fails
+    console.error(`Error: ${error.message}`);
+    process.exit(1); 
   }
 };
 
-export default connectMongoDB;
+export default connect_to_db;
